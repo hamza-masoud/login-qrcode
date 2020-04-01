ca7e9aa044be0ae0051ab5d58e7fbae3f1042ad6
hamza masoud <hamzahanimasoud@gmail.com>
2020-04-01 18:44:39 +0300

    all right but the link in field test No.22

--- App/backend/settings.py
+++ App/backend/settings.py
@@ -10,8 +10,8 @@ app.config.app_url = "https://login-qrcode.herokuapp.com/"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-basedir.split(os.sep)
-del basedir[0]
+basedir = basedir.split(os.sep)
+basedir.remove(basedir[0])
 basedir = os.sep.join(basedir)
 
 download_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'QRimgs'))
