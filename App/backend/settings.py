54d9366a2e29019d66452fe2e860600127f0a957
hamza masoud <hamzahanimasoud@gmail.com>
2020-04-05 22:46:58 +0300

    edit showroom to check if the img exist

--- App/backend/settings.py
+++ App/backend/settings.py
@@ -16,7 +16,7 @@ download_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Q
 app.config.file_name_length = 5
 
 db_path = os.path.join(basedir, 'projectDB.db')
-
+ 
 db_uri = 'sqlite:///{}'.format(db_path)
 # app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ezssqjvyoievzv:6bfd14bad593f36b68cc3880156673f4e8310b9039f2e0d0c98fc2d09def741b@ec2-52-71-85-210.compute-1.amazonaws.com:5432/deo6bpel8ivas1'
