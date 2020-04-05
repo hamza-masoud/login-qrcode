0547f669896f8ef45671eab141e793f2175f9520
hamza masoud <hamzahanimasoud@gmail.com>
2020-04-05 22:22:33 +0300

    edit showroom to check if the img exist

--- App/backend/Controllers/adminController.py
+++ App/backend/Controllers/adminController.py
@@ -116,6 +116,12 @@ class adminController:
                         users.append([z, name.name, name.username, user.time_login])
                         z += 1
 
+                QR_url = os.path.join(basedir, "static", "imgs", "QRimg", roomid + ".png")
+                if os.path.isfile(QR_url):
+                    pass
+                else:
+                    create_QR(roomid)
+
                 return render_template("pages/admin/admin_Room.html", room=room, users=users,
                                        imgurl=roomid + ".png")
         return redirect(app.config.app_url + "admin/")
@@ -135,6 +141,6 @@ def create_QR(QR_code):
     qr = qrcode.make(QR_code)
     basedir = os.path.abspath(os.path.dirname(__file__))
     QR_url = os.path.join(basedir, "static", "imgs", "QRimg", QR_code + ".png")
-    
+
     qr.save(QR_url)
     return QR_url
--- App/backend/settings.py
+++ App/backend/settings.py
@@ -7,9 +7,7 @@ app.secret_key = "data_convert"
 app.config.app_url = "https://login-qrcode.herokuapp.com/"
 # app.config.app_url = "http://127.0.0.1:5000/"
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 download_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'QRimgs'))
 
@@ -19,7 +17,8 @@ db_path = os.path.join(basedir, 'projectDB.db')
 
 db_uri = 'sqlite:///{}'.format(db_path)
 # app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ezssqjvyoievzv:6bfd14bad593f36b68cc3880156673f4e8310b9039f2e0d0c98fc2d09def741b@ec2-52-71-85-210.compute-1.amazonaws.com:5432/deo6bpel8ivas1'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgres://ezssqjvyoievzv:6bfd14bad593f36b68cc3880156673f4e8310b9039f2e0d0c98fc2d09def741b@ec2-52-71-85-210.compute-1.amazonaws.com:5432/deo6bpel8ivas1'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 app.config['UPLOAD_FOLDER'] = 'QRimgs'
