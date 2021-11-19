0a23448de2ac61e3eba875a01366b814540670b3
hamza masoud <hamzahanimasoud@gmail.com>
2021-11-19 16:24:18 +0200

    add Flask-Migrate to req

--- App/__init__.py
+++ App/__init__.py
@@ -5,5 +5,8 @@ app = Flask(__name__,
             template_folder=os.path.join("frontend", "templates"),
             static_folder=os.path.join("frontend", "static"))
 
+from App.backend.models import db, migrate
+
+
 from App.backend import settings
 from App.backend import route
